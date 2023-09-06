fb8621dfc53e2338cb4e54eb4fcac9e1bc810d55
Franco Quiroga <109109493+FrancooQ@users.noreply.github.com>
2023-09-06 16:29:04 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import express from 'express'
 import handlebars from 'express-handlebars'
 import __dirname from './utils.js'
-import {Server} from 'socket.io';
 
 import cartsRoute from './routes/carts.js';
 import productsRoute from './routes/products.js';
@@ -9,10 +8,6 @@ import productsRoute from './routes/products.js';
 
 const app = express();
 const port=8080;
-
-app.engine('handlebars', handlebars.engine())
-app.set('views', __dirname + '/views')
-app.set('view engine', 'handlebars')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
@@ -25,8 +20,3 @@ const httpServer = app.listen(port, () => {
     console.log('Server ON')
 })
 
-const io = new Server(httpServer);
-
-io.on('connection', socket => {
-    console.log("Nuevo cliente conectado.");
-})
