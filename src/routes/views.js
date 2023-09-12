ab3968673649138e2ee88ed550ad38dc9b0ce0a4
Franco Quiroga <francoags02@gmail.com>
2023-09-12 20:32:54 -0300

    Actualizo home handlebars

--- src/routes/views.js
+++ src/routes/views.js
@@ -4,12 +4,12 @@ import ProductManager from "../dao/managers/ProductManager.js";
 const router = Router();
 const PM = new ProductManager();
 
-router.get('/', (req, res) => {
-    const products = PM.getProducts();
+router.get('/', async (req, res) => {
+    const products = await PM.getProducts();
     res.render("home", {products, style: "index.css"});
 })
 
-router.get('/realtimeproducts', (req, res) => {
+router.get('/realtimeproducts', async (req, res) => {
     res.render("realTimeProducts", {});
 })
 
